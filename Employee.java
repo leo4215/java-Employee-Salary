3d4393a48b692f4479544bee515927b5051c6b5c
Leonardo de Camargo <134113848+leo4215@users.noreply.github.com>
2023-10-26 15:24:46 -0400

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,16 @@
+public class Employee {
+    public String name;
+    public double grossSalary, tax;
+
+    public double netSalary(){
+        return grossSalary - tax;
+    }
+    
+    public void increaseSalary(double percentage){
+        grossSalary += percentage/100 * grossSalary;
+    }
+
+    public String toString(){
+        return name + ", $ " + String.format("%.2f", netSalary());
+    }
+}
